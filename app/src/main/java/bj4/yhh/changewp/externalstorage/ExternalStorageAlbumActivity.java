ca18cd0dc66f78aaabaad212e7c7c5e5325ce02c
Yen-Hsun, Huang <s011208@gmail.com>
2017-04-05 13:40:31 +0800

    add request permission text

--- app/src/main/java/bj4/yhh/changewp/externalstorage/ExternalStorageAlbumActivity.java
+++ app/src/main/java/bj4/yhh/changewp/externalstorage/ExternalStorageAlbumActivity.java
@@ -1,16 +1,21 @@
 package bj4.yhh.changewp.externalstorage;
 
 import android.Manifest;
+import android.content.Intent;
 import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.List;
@@ -29,8 +34,11 @@ public class ExternalStorageAlbumActivity extends AppCompatActivity implements A
     private static final boolean DEBUG = true;
 
     private static final int REQUEST_PERMISSION_GET_READ_EXTERNAL_STORAGE = 10001;
+    private static final int REQUEST_PERMISSION_ACTIVITY = 10002;
 
     private final Handler mHandler = new Handler();
+
+    private TextView mRequestPermissionTextView;
 
     private AlbumView mAlbumView;
     private Map<String, List<ImageData>> mExternalStorageImageDataMap = new HashMap<>();
@@ -53,6 +61,18 @@ public class ExternalStorageAlbumActivity extends AppCompatActivity implements A
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mRequestPermissionTextView = (TextView) findViewById(R.id.permission_request_text);
+        mRequestPermissionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivityForResult(intent, REQUEST_PERMISSION_ACTIVITY);
+            }
+        });
+
         mAlbumView = (AlbumView) findViewById(R.id.album_view);
         mAlbumView.setEnableSpan(true)
                 .setEnableGridMargin(true)
@@ -66,9 +86,19 @@ public class ExternalStorageAlbumActivity extends AppCompatActivity implements A
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PERMISSION_ACTIVITY) {
+            if (EasyPermissions.hasPermissions(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                updateAlbumViewDataList();
+            }
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        updateAlbumViewDataList();
     }
 
     @Override
@@ -87,12 +117,17 @@ public class ExternalStorageAlbumActivity extends AppCompatActivity implements A
         Log.d(TAG, "updateAlbumViewDataList");
         if (EasyPermissions.hasPermissions(
                 this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            mRequestPermissionTextView.setVisibility(View.INVISIBLE);
+            mAlbumView.setVisibility(View.VISIBLE);
+
             mExternalStorageImageDataMap.clear();
             mExternalStorageImageDataMap.putAll(Utility.groupImageDataByFolder(Utility.getAllExternalStorageImageData(ExternalStorageAlbumActivity.this)));
             mAlbumView.setImageDataList(Utility.getFirstImageDataFromGroup(mExternalStorageImageDataMap));
         } else {
-            EasyPermissions.requestPermissions(this, getString(R.string.external_storage_activity_read_loca_file_permission),
+            EasyPermissions.requestPermissions(this, getString(R.string.external_storage_activity_read_local_file_permission),
                     REQUEST_PERMISSION_GET_READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            mRequestPermissionTextView.setVisibility(View.VISIBLE);
+            mAlbumView.setVisibility(View.INVISIBLE);
         }
     }
 
