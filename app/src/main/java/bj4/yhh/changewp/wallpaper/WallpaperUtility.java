c18dda8649dafdd5beaddbd5e819cf75ca27364f
s011208 <s011208@gmail.com>
2017-04-15 00:13:58 +0800

    user firebase job scheduler instead

--- app/src/main/java/bj4/yhh/changewp/wallpaper/WallpaperUtility.java
+++ app/src/main/java/bj4/yhh/changewp/wallpaper/WallpaperUtility.java
@@ -1,14 +1,13 @@
 package bj4.yhh.changewp.wallpaper;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
+
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
 
 import bj4.yhh.changewp.settings.main.WallpaperTimeInterval;
 import bj4.yhh.changewp.wallpaper.scheduler.WallpaperJobService;
@@ -26,22 +25,14 @@ public class WallpaperUtility {
         if (DEBUG) {
             Log.v(TAG, "scheduleWallpaperTask, interval: " + interval);
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            JobInfo.Builder builder = new JobInfo.Builder(WallpaperJobService.JOB_CHANGE_WALLPAPER,
-                    new ComponentName(context.getPackageName(), WallpaperJobService.class.getName()))
-                    .setPersisted(true)
-                    .setPeriodic(interval);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+        Job job = dispatcher.newJobBuilder().setService(WallpaperJobService.class)
+                .setTag(WallpaperJobService.JOB_CHANGE_WALLPAPER)
+                .setRecurring(true)
+                .setLifetime(Lifetime.FOREVER)
+                .setTrigger(Trigger.executionWindow(0, (int) interval))
+                .build();
 
-            JobScheduler scheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-            scheduler.cancelAll();
-            scheduler.schedule(builder.build());
-        } else {
-            AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            Intent intent = new Intent(context, ChangeWallpaperService.class);
-            intent.putExtra(ChangeWallpaperService.EXTRA_FROM_JOB_SCHEDULER, ChangeWallpaperService.FROM_ALARM_MANAGER);
-            PendingIntent alarmIntent = PendingIntent.getService(context, 3000, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            alarmMgr.cancel(alarmIntent);
-            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + interval, interval, alarmIntent);
-        }
+        dispatcher.mustSchedule(job);
     }
 }
--- app/src/main/java/bj4/yhh/changewp/wallpaper/scheduler/WallpaperJobService.java
+++ app/src/main/java/bj4/yhh/changewp/wallpaper/scheduler/WallpaperJobService.java
@@ -1,11 +1,12 @@
 package bj4.yhh.changewp.wallpaper.scheduler;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
 
 import bj4.yhh.changewp.wallpaper.ChangeWallpaperService;
 
@@ -15,17 +16,16 @@ import bj4.yhh.changewp.wallpaper.ChangeWallpaperService;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class WallpaperJobService extends JobService {
+    public static final String JOB_CHANGE_WALLPAPER = "Change-wallpaper";
     private static final boolean DEBUG = true;
     private static final String TAG = "WallpaperJobService";
-
-    public static final int JOB_CHANGE_WALLPAPER = 10000;
 
     @Override
     public boolean onStartJob(JobParameters params) {
         if (DEBUG) {
             Log.d(TAG, "onStartJob");
         }
-        if (JOB_CHANGE_WALLPAPER == params.getJobId()) {
+        if (JOB_CHANGE_WALLPAPER.equals(params.getTag())) {
             Intent intent = new Intent(this, ChangeWallpaperService.class);
             intent.putExtra(ChangeWallpaperService.EXTRA_FROM_JOB_SCHEDULER, ChangeWallpaperService.FROM_JOB_SCHEDULER);
             startService(intent);
